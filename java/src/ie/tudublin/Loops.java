65dfa357cfcd286fc5846203e56abdbd43b8103e
C20338661 Ivan Medved <c20338661@mytudublin.ie>
2022-02-09 21:29:01 +0000

    lab3_part1

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -12,11 +12,20 @@ public class Loops extends PApplet
 
 	public void setup() {
 		colorMode(HSB);
-		
 	}
 
 	
 	public void draw()
 	{	
+		int a = 0;
+		int b = 50;
+		int color = 0;
+		for(int i=1; i<=10;i++){
+			fill(color);
+			rect(a, 0, b, 500);
+			a = a + 50;
+			b = b + 50;
+			color = color + 30;
+		}
 	}
 }
