aaa0bec3b9de64082d29fee84adda0dcddaf8f16
C20338661 Ivan Medved <c20338661@mytudublin.ie>
2022-02-14 12:52:05 +0000

    class done

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -2,96 +2,105 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class Loops extends PApplet
-{
-
+public class Loops extends PApplet {
 
 	int mode = 0;
 
-	public void settings()
-	{
+	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
 		colorMode(HSB);
-		
+
 	}
 
-	public void keyPressed()
-	{
-		if (key >= '0' && key <='9')
-		{
+	public void keyPressed() {
+		if (key >= '0' && key <= '9') {
 			mode = key - '0';
 		}
 		println(mode);
 	}
 
-<<<<<<< HEAD
-=======
-	float magicMap(float a, float b, float c, float d, float e)
-	{
+	float magicMap(float a, float b, float c, float d, float e) {
 		float output;
 		a -= b;
 		c -= b;
-		e-= d;
+		e -= d;
 
-		output = ((a/c)*e) + d;
+		output = ((a / c) * e) + d;
 
 		return output;
 	}
 
-	float magicMap1(float a, float b, float c, float d, float e)
-	{
-		float r1 = c -b;
-		float r2 = e-d;
+	float magicMap1(float a, float b, float c, float d, float e) {
+		float r1 = c - b;
+		float r2 = e - d;
 		float howFar = a - b;
 
 		return d + ((howFar / r1) * r2);
 	}
 
->>>>>>> master
-	
-	public void draw()
-	{
-		
-		switch(mode)	
-		{
+	float offset = 0;
+
+	public void draw() {
+
+		switch (mode) {
 			case 0:
 				background(0);
 				int bars = (int) (mouseX / 20.0f);
-<<<<<<< HEAD
-				float w = width / (float)bars;
-				for(int i = 0; i<bars ; i++)
-				{
-					noStroke();
-					fill(map(i, 0, bars, 0, 255),255,255);
-					rect(map(i,0,bars,0,500),0,w,height);
-				}
-
-				break;
-			case 1:
-				break;
-=======
-				float w = width / (float)bars;	
-				for(int i = 0 ; i < bars ; i ++)
-				{
+				float w = width / (float) bars;
+				for (int i = 0; i < bars; i++) {
 					noStroke();
 					fill(map(i, 0, bars, 0, 255), 255, 255);
 					rect(map(i, 0, bars, 0, 500), 0, w, height);
 				}
 				break;
 			case 1:
-			
+				background(0);
+				int squares = (int) (mouseX / 20.0f);
+				float h = width / (float) squares;
+				for (int i = 0; i < squares; i++) {
+					noStroke();
+					fill(map(i, 0, squares, 0, 255), 255, 255);
+					float x = map(i, 0, squares, 0, width);
+					rect(x, x, h, h);
+					rect((width - h) - x, x, h, h);
+				}
 				break;
-				//map(a,b,c,d,e);
-				//a = inputvalue
-				// b - c - start and end of the first range
-				// d, e 0 - start and and of the end range
+			// map(a,b,c,d,e);
+			// a = inputvalue
+			// b - c - start and end of the first range
+			// d, e 0 - start and and of the end range
 
-				// map(-2, 10, 90, 200, 233);
+			// map(-2, 10, 90, 200, 233);
+			case 2:
+				background(0);
+				int circles = (int) (mouseX / 20.0f);
+				float d = width / (float) circles;
+				for (int i = 0; i < circles; i++) {
+					noStroke();
+					fill(map(i, 0, circles, 0, 255), 255, 255);
+					circle(map(i, 0, circles - 1, d / 2.0f, width - (d / 2.0f)), height / 2, d);
+				}
 
->>>>>>> master
+				break;
+			case 3:
+				background(0);
+				int circles2 = (int) (mouseX / 20.0f);
+				offset += (mouseY / 100.0f);
+				float diamater = width / (float) circles2;
+				for (int j = 0; j < circles2; j++) {
+					for (int i = 0; i < circles2; i++) {
+						noStroke();
+						float c = map((i+j+offset), 0, circles2 * 2, 0, 255) % 256;
+						fill(c, 255, 255);
+						float x = map(i, 0, circles2 - 1, diamater / 2.0f, width - (diamater / 2.0f));
+						float y = map(j, 0, circles2 - 1, diamater / 2.0f, width - (diamater / 2.0f));
+						circle(x, y, diamater);
+					}
+				}
+
 		}
 	}
 }
