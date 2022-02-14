8318965ab4a391be38600ba1018980f7ad436457
C20338661 Ivan Medved <c20338661@mytudublin.ie>
2022-02-14 11:44:32 +0000

    first exercise

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,6 +5,9 @@ import processing.core.PApplet;
 public class Loops extends PApplet
 {
 
+
+	int mode = 0;
+
 	public void settings()
 	{
 		size(500, 500);
@@ -15,8 +18,35 @@ public class Loops extends PApplet
 		
 	}
 
+	public void keyPressed()
+	{
+		if (key >= '0' && key <='9')
+		{
+			mode = key - '0';
+		}
+		println(mode);
+	}
+
 	
 	public void draw()
-	{	
+	{
+		
+		switch(mode)	
+		{
+			case 0:
+				background(0);
+				int bars = (int) (mouseX / 20.0f);
+				float w = width / (float)bars;
+				for(int i = 0; i<bars ; i++)
+				{
+					noStroke();
+					fill(map(i, 0, bars, 0, 255),255,255);
+					rect(map(i,0,bars,0,500),0,w,height);
+				}
+
+				break;
+			case 1:
+				break;
+		}
 	}
 }
